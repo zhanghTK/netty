598cfd56d7c447fc0ffd054a8701a5fa8bc9776c
ZhanghTk <510223064@qq.com>
2017-07-17 21:37:13 +0800

    add netty demo

---
+++ src/main/java/tk/zhangh/netty/ch3/TimeClientHandler.java
@@ -0,0 +1,41 @@
+package tk.zhangh.netty.ch3;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by ZhangHao on 2017/7/17.
+ */
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+    private final ByteBuf firstMessage;
+
+    public TimeClientHandler() {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+
+        String body = new String(req, "UTF-8");
+        System.out.println("Now is : " + body);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
